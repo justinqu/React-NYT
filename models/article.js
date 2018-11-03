3b57cc81a74c6edba6cf6096ec158267610b41da
Justin Quinonez <justinqnns@gmail.com>
2018-11-02 23:45:43 -0400

    updates

---
+++ models/article.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  date:{type:String, default: Date.now},
+  url: { type: String, required: true },
+
+
+});
+
+const Atricle = mongoose.model("Article", articleSchema);
+
+module.exports = Atricle;
