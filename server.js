c7f64d5072c647083def3858156e3d29229b2b62
justinqu <31523249+justinqu@users.noreply.github.com>
2018-11-02 23:43:44 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+  {
+    useMongoClient: true
+  }
+);
+
+/** DB */
+const db = require('./models')
+const { Article } = db
+/** END DB */
+
+
+/** routes */
+app.post("/api/saved", (req, res) => {
+  // get the posted object
+  var article = req.body
+
+  // call Article.create
+  // then return some json (success|error)
+  Article.create(article)
+  .then(() => {
+    res.json(article)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
+})
+
+app.get('/api/saved', (req, res) => {
+  Article.find({}).then(articles => res.json(articles))
+})
+/** end routes */
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
